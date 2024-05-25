1673543f475a0118db8c222286f6a68eecb98d7c
Anand Arumugam <anaaru@microsoft.com>
2024-05-25 12:02:36 -0700

    Add help to download-t2build

--- Download-T2Build.ps1
+++ Download-T2Build.ps1
@@ -1,3 +1,50 @@
+<#
+.SYNOPSIS
+Download-T2Build.ps1 will download a specific build of new Teams client from the Teams CDN.
+You can also install the downloaded build on Windows by using the -Install switch.
+
+.DESCRIPTION
+Download-T2Build.ps1 will download a specific build of new Teams client from the Teams CDN
+for the specified ring and architecture. You can also install the downloaded build on Windows 
+by using the -Install switch. The script will download the build to the Downloads folder.
+You can use the -dryRun switch to see the download command without actually downloading the build.
+
+.PARAMETER build
+The desktop client version to download.
+
+.PARAMETER platform
+The platform to download the build for. Valid values are "windows" and "osx".
+
+.PARAMETER ring
+The ring to download the build for. Valid values are "r0", "r1", "r2", "r3", and "r4".
+If not specified, the default value is "r0".
+
+.PARAMETER arch
+The architecture to download the build for. Valid values are "x64", "arm64", and "x86".
+If not specified, the default value is "x64".
+
+.PARAMETER dryRun
+Show the download command without actually downloading the build.
+
+.PARAMETER Install
+If specified, it installs the downloaded build on Windows.
+
+.EXAMPLE
+To download a specific build of the Teams client from a specific ring:
+Download-T2Build.ps1 -build 12345.6000.0789.1234 -ring r4
+
+.EXAMPLE
+To download and install a specific build of the Teams client from a specific ring:
+Download-T2Build.ps1 -build 12345.6000.0789.1234 -ring r4 -Install
+
+.EXAMPLE
+To download and do a dry run of a specific build of the Teams client from a specific ring:
+Download-T2Build.ps1 -build 12345.6000.0789.1234 -ring r4 -dryRun
+
+.NOTES
+Any additional notes about the script.
+#>
+
 Param(
   [Parameter(mandatory=$true)]
   [string] $build,
@@ -10,7 +57,8 @@ Param(
   [Parameter()]
   [ValidateSet("x64","arm64","x86")]
   [string] $arch = "x64",
-  [Switch] $dryRun = $false)
+  [Switch] $dryRun = $false,
+  [Switch] $Install = $false)
 
 if ($build -eq $nul) {
   Write-Output "Usage: Download-T2ClientBuild.ps1 -build 12345.6000.0789.1234 -ring r3"
@@ -35,3 +83,12 @@ $downloadPath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads"
 $pkgFile = Join-Path -Path $downloadPath -ChildPath $appPkgName
 Invoke-WebRequest -Uri $buildUrl -OutFile $pkgFile
 Write-Output "Downloaded $pkgFile"
+
+if ($Install) {
+  Write-Information "Installing $pkgFile..."
+  if ($platform -eq "windows") {
+    Add-AppxPackage -Path $pkgFile
+  } else {
+    Write-Error "Installation is not supported on this platform."
+  }
+}
