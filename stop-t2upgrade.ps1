cad431a162a46453afdecb53c7b2c62e15add9da
anaaru <anaaru@microsoft.com_msteamsmdb>
2025-03-04 14:10:18 -0800

    Enable/disable wv2 pre-rel runtime

--- stop-t2upgrade.ps1
+++ stop-t2upgrade.ps1
@@ -1,6 +1,14 @@
 param(
-  [switch]$Undo
+  [switch]$Undo,
+  [switch]$useStableWv2,
+  [switch]$usePreRelWv2 = $false
 )
+
+# Ensure useStableWv2 and usePreRelWv2 are mutually exclusive
+if ($useStableWv2 -and $usePreRelWv2) {
+  Write-Error "You cannot use both -useStableWv2 and -usePreRelWv2 switches at the same time."
+  exit 1
+}
 
 if ($Undo) {
   $configJsonPath = "{0}\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\configuration.json" -f $env:LOCALAPPDATA
@@ -26,5 +34,18 @@ else {
   $configJsonPath = "{0}\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams" -f $env:LOCALAPPDATA
   New-Item -Path $configJsonPath -ItemType Directory -Force
   $stopUpdateJsonStr = "{'x64/buildLink': '', 'x64/latestVersion': '', 'core/devMenuEnabled': true, 'logging/minimumSeverity': 'DebugPII'}"
-  $stopUpdateJsonStr | ConvertFrom-Json | ConvertTo-Json | Out-File -FilePath $configJsonPath\configuration.json
+  $configObject = $stopUpdateJsonStr | ConvertFrom-Json
+
+  if ($usePreRelWv2) {
+    $configObject.'webview/usePreReleaseWebview2Runtime' = $true
+  } else {
+    $configObject.'webview/usePreReleaseWebview2Runtime' = $false
+  }
+
+  if ($useStableWv2 -and $configObject.'webview/usePreReleaseWebview2Runtime' -eq $true) {
+    $configObject.'webview/usePreReleaseWebview2Runtime' = $false
+  }
+
+  $configJson = $configObject | ConvertTo-Json
+  $configJson | Out-File -FilePath $configJsonPath\configuration.json
 }
